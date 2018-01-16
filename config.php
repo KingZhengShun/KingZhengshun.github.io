aa1d0e23117c75c66c74158b12f3c7d05217a74b
KingZhengShun <zhenshun008@hotmail.com>
2018-01-17 01:20:11 +0800

    PHP学习

---
+++ config.php
@@ -0,0 +1,6 @@
+<?php
+header('Content-Type: text/html; charset=UTF-8');
+
+define( "WB_AKEY" , '3325651325' );
+define( "WB_SKEY" , '920d163f659628859afd5ebc7555b583' );
+define( "WB_CALLBACK_URL" , 'http://localhost/PHPStudy/PHP_02/callback.php' );
